ad8ba5d40603576eb256f6d7b0dea895eadcfd5f
Adarsha1999 <36310001+Adarsha1999@users.noreply.github.com>
2023-06-28 22:11:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
 }
 resource "aws_s3_bucket" "example" {
   bucket = "my-test-22062023"
